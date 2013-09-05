490818d029148a74d9f9730574cf1412db250dff
Kris Foster <kris.foster@gmail.com>
2013-09-04 19:07:18 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+
+setup(name='etcd-py',
+        version='0.0.1',
+        description='Client for Etcd',
+        author='Kris Foster',
+        author_email='kris.foster@gmail.com',
+        url='https://github.com/transitorykris/etcd-py',
+        packages=['etcd'],
+        )
