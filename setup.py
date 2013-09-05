4c3e68994095b67ab1b029e82d02128162c5ead0
Kris Foster <kris.foster@gmail.com>
2013-09-05 00:38:33 -0700

    bump to v0.0.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("LICENSE") as file:
     license = file.read()
 
 setup(name="etcd-py",
-        version="0.0.1",
+        version="0.0.2",
         description="Client for Etcd",
         long_description=long_description,
         author="Kris Foster",
